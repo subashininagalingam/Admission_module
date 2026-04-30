dc451a77b84b9722e733babbdce99fdfa16a30e7
subashininagalingam <subanagalingam.dev@gmail.com>
2026-04-30 16:21:08 +0530

    Refactor: create_superuser.py

--- csc_crm/create_superuser.py
+++ csc_crm/create_superuser.py
@@ -1,17 +1,14 @@
+from django.contrib.auth import get_user_model
 import os
-import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectname.settings')
-django.setup()
+User = get_user_model()
 
-from django.contrib.auth.models import User
-
-username = "admin"
-email = "admin@gmail.com"
-password = "admin123"
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
 if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
+    User.objects.create_superuser(username=username, email=email, password=password)
     print("Superuser created successfully")
 else:
     print("Superuser already exists")
