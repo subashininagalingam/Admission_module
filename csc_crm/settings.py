bcec9f33c37cc4838a67431667d0643b7a7293e5
subashininagalingam <subanagalingam.dev@gmail.com>
2026-05-06 15:19:40 +0530

    Fix: Migrations issuse

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -76,7 +76,6 @@ WSGI_APPLICATION = 'csc_crm.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
