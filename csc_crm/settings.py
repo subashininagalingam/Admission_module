b04f7263adce6832b4fc7daca647fefe3267dc80
subashininagalingam <subanagalingam.dev@gmail.com>
2026-06-17 12:04:13 +0530

    chore: ready to deploy

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -87,16 +87,16 @@ DATABASES = {
    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CSC_CRM',
-        'USER' :'postgres',
-        'PASSWORD' :'suba',
-        'HOST' :'localhost',
-        'PORT' :'5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'CSC_CRM',
+#         'USER' :'postgres',
+#         'PASSWORD' :'suba',
+#         'HOST' :'localhost',
+#         'PORT' :'5432',
+#     }
+# }
 
 
 # Password validation
