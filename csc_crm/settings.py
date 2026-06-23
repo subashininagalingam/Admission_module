24e77e0b6ab2ae6eb9d40f91430e71a1215bdf29
subashininagalingam <subanagalingam.dev@gmail.com>
2026-06-23 09:28:45 +0530

    Refactor: Smtp value changed

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -158,12 +158,12 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 
-# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-EMAIL_HOST_USER = "gopikas04082005@gmail.com"
-EMAIL_HOST_PASSWORD = "qmfhpjepymvbypra"
+# EMAIL_HOST_USER = "gopikas04082005@gmail.com"
+# EMAIL_HOST_PASSWORD = "qmfhpjepymvbypra"
 
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
